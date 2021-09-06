6adacf26e74823ebc4160d9dab5a742ce2bc68ab
icosagon8 <dmpavlov8@yandex.ru>
2021-09-06 13:58:17 +0300

    feat: add an opportunity to override Material-UI's styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
+import { StylesProvider } from '@material-ui/core/styles';
 import { Header } from './components/Header/Header';
 
 export function App(): JSX.Element {
-  return <Header />;
+  return (
+    <StylesProvider injectFirst>
+      <Header />
+    </StylesProvider>
+  );
 }
