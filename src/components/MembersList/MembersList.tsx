f60625a99ca15db9cc38f5460dd71868a943ae2d
MaxZay <maximzay2017@gmail.com>
2021-09-09 12:32:17 +0300

    feat: add member list component

---
+++ src/components/MembersList/MembersList.tsx
@@ -0,0 +1,19 @@
+import MemberCardType from '../../models/iMemberCard';
+import { MemberCard } from '../MemberCard/MemberCard';
+import './MembersList.scss';
+
+interface ImemberList {
+  data: MemberCardType[];
+}
+
+export const MembersList = (props: ImemberList): JSX.Element => {
+  const { data } = props;
+  return (
+    <div className="member-list">
+      <h2 className="member-list__title">Members</h2>
+      <div className="member-list__wrapper">
+        {data.length > 0 && data.map((item) => <MemberCard src={item.src} name={item.name} />)}
+      </div>
+    </div>
+  );
+};
--- src/pages/LobbyPage/LobbyPage.tsx
+++ src/pages/LobbyPage/LobbyPage.tsx
@@ -1,12 +1,27 @@
+import { MembersList } from '../../components/MembersList/MembersList';
 import { StartGame } from '../../components/StartGame/StartGame';
 import { Title } from '../../components/Title/Title';
 import './LobbyPage.scss';
+
+const data = [
+  { src: 'https://www.meme-arsenal.com/memes/732cac98dc1d07127952687091bf7005.jpg', name: 'Sung-Jin-Woo' },
+  { src: 'https://www.meme-arsenal.com/memes/732cac98dc1d07127952687091bf7005.jpg', name: 'Sung-Jin-Woo' },
+  { src: 'https://www.meme-arsenal.com/memes/732cac98dc1d07127952687091bf7005.jpg', name: 'Sung-Jin-Woo' },
+  { src: 'https://www.meme-arsenal.com/memes/732cac98dc1d07127952687091bf7005.jpg', name: 'Sung-Jin-Woo' },
+  { src: 'https://www.meme-arsenal.com/memes/732cac98dc1d07127952687091bf7005.jpg', name: 'Sung-Jin-Woo' },
+  { src: 'https://www.meme-arsenal.com/memes/732cac98dc1d07127952687091bf7005.jpg', name: 'Sung-Jin-Woo' },
+  { src: 'https://www.meme-arsenal.com/memes/732cac98dc1d07127952687091bf7005.jpg', name: 'Sung-Jin-Woo' },
+  { src: 'https://www.meme-arsenal.com/memes/732cac98dc1d07127952687091bf7005.jpg', name: 'Sung-Jin-Woo' },
+  { src: 'https://www.meme-arsenal.com/memes/732cac98dc1d07127952687091bf7005.jpg', name: 'Sung-Jin-Woo' },
+  { src: 'https://www.meme-arsenal.com/memes/732cac98dc1d07127952687091bf7005.jpg', name: 'Sung-Jin-Woo' },
+];
 
 export const LobbyPage = (): JSX.Element => {
   return (
     <div className="lobby-page">
       <Title title="Spring 23 planning (issues 13, 533, 5623, 3252, 6623, ...)" />
       <StartGame />
+      <MembersList data={data} />
     </div>
   );
 };
