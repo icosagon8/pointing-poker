1efc87ac252172902f14c62d3c4b493567a1c25a
MaxZay <maximzay2017@gmail.com>
2021-09-09 12:51:47 +0300

    feat: add clipboard

--- src/components/StartGame/StartGame.tsx
+++ src/components/StartGame/StartGame.tsx
@@ -1,16 +1,30 @@
 import { Button } from '@material-ui/core';
+import { ChangeEvent, useState } from 'react';
 import { MemberCard } from '../MemberCard/MemberCard';
 import './StartGame.scss';
 
 export const StartGame = (): JSX.Element => {
+  const [linkValue, setLinkValue] = useState<string>('http://pockerplanning.c...');
+
   return (
     <div className="start-game">
       <h4 className="start-game__scram-master">Scram master:</h4>
       <MemberCard src="https://www.meme-arsenal.com/memes/732cac98dc1d07127952687091bf7005.jpg" name="Sung-Jin-Woo" />
       <h3 className="start-game__to-lobby">Link to lobby:</h3>
       <div className="start-game__link-block">
-        <input className="start-game__input" value="http://pockerplanning.c..." />
-        <Button variant="contained" color="primary" className="start-game__btn start-game__copy">
+        <input
+          className="start-game__input"
+          value={linkValue}
+          onChange={(event: ChangeEvent<HTMLInputElement>) => {
+            setLinkValue(event.target.value);
+          }}
+        />
+        <Button
+          variant="contained"
+          color="primary"
+          className="start-game__btn start-game__copy"
+          onClick={() => navigator.clipboard.writeText(linkValue)}
+        >
           Copy
         </Button>
       </div>
