c3dba2c69d8989e9fe15a16c5ce4faea2402f8b0
MaxZay <maximzay2017@gmail.com>
2021-09-09 14:24:33 +0300

    fix: change member list title

--- src/components/MembersList/MembersList.tsx
+++ src/components/MembersList/MembersList.tsx
@@ -10,7 +10,7 @@ export const MembersList = (props: ImemberList): JSX.Element => {
   const { data } = props;
   return (
     <div className="member-list">
-      <h2 className="member-list__title">Members</h2>
+      <h2 className="member-list__title">Members:</h2>
       <div className="member-list__wrapper">
         {data.length > 0 && data.map((item) => <MemberCard src={item.src} name={item.name} />)}
       </div>
--- src/pages/LobbyPage/LobbyPage.tsx
+++ src/pages/LobbyPage/LobbyPage.tsx
@@ -1,3 +1,4 @@
+import { Issues } from '../../components/Issues/Issues';
 import { MembersList } from '../../components/MembersList/MembersList';
 import { StartGame } from '../../components/StartGame/StartGame';
 import { Title } from '../../components/Title/Title';
@@ -22,6 +23,7 @@ export const LobbyPage = (): JSX.Element => {
       <Title title="Spring 23 planning (issues 13, 533, 5623, 3252, 6623, ...)" />
       <StartGame />
       <MembersList data={data} />
+      <Issues />
     </div>
   );
 };
