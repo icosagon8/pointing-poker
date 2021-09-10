afb0b19fb4376d7f281dbf8425468106f9f839a5
icosagon8 <dmpavlov8@yandex.ru>
2021-09-10 20:48:13 +0300

    feat: add to the server socket listener for sending messages in chat

--- server/app.ts
+++ server/app.ts
@@ -1,14 +1,30 @@
 import cors from 'cors';
 import express from 'express';
+import { createServer } from 'http';
+import { Server, Socket } from 'socket.io';
 
 const PORT = process.env.PORT || 8080;
 const app = express();
 app.use(cors());
 
+const server = createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST'],
+  },
+});
+
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.listen(PORT, () => {
+io.on('connection', (socket: Socket) => {
+  socket.on('message', (message) => {
+    io.emit('message', message);
+  });
+});
+
+server.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`);
 });
