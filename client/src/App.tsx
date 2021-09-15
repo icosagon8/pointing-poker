46c088d1ef629f578f581d418130b5837e0a2a68
icosagon8 <dmpavlov8@yandex.ru>
2021-09-15 19:53:27 +0300

    feat: add Home page to the App

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,12 @@
 import { StylesProvider } from '@material-ui/core/styles';
 import { Header } from './components/Header/Header';
+import { Home } from './pages/Home/Home';
 
 export function App(): JSX.Element {
   return (
     <StylesProvider injectFirst>
       <Header />
+      <Home />
     </StylesProvider>
   );
 }
