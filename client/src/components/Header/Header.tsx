a153d1ec232244d4acbcd3604ee1ea9c6c174621
MaxZay <maximzay2017@gmail.com>
2021-09-16 23:08:01 +0300

    feat: connect chat

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -1,15 +1,28 @@
 import './Header.scss';
 import { AppBar, IconButton, Toolbar, Container } from '@material-ui/core';
 import ChatOutlinedIcon from '@material-ui/icons/ChatOutlined';
+import { useAppSelector, useAppDispatch } from '../../store/hooks/hooks';
 import { Logo } from '../Logo/Logo';
+import { on, off } from '../../store/slices/chatSlice';
 
 export function Header(): JSX.Element {
+  const isOpen = useAppSelector((state) => state.chat.isOpen);
+  const dispatch = useAppDispatch();
+
+  const clickHandler = () => {
+    if (isOpen) {
+      dispatch(off());
+    } else {
+      dispatch(on());
+    }
+  };
+
   return (
     <AppBar className="header" position="static">
       <Container>
         <Toolbar className="header__wrapper" disableGutters>
           <Logo className="header__logo" />
-          <IconButton className="header__chat-btn" color="inherit">
+          <IconButton onClick={clickHandler} className="header__chat-btn" color="inherit">
             <ChatOutlinedIcon />
           </IconButton>
         </Toolbar>
