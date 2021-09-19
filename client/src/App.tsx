b7d38ce4ac10bee93cb2f11da1a62b4613f5dbe4
icosagon8 <dmpavlov8@yandex.ru>
2021-09-19 22:03:06 +0300

    feat: update contexts of the App

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,20 +3,26 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { SocketProvider } from './socketContext';
 import { Header } from './components/Header/Header';
 import { ROUTES } from './routes';
+import { MainProvider } from './mainContext';
+import { UsersProvider } from './usersContext';
 
 export function App(): JSX.Element {
   return (
     <StylesProvider injectFirst>
-      <SocketProvider>
-        <Router>
-          <Header />
-          <Switch>
-            {ROUTES.map((route) => (
-              <Route path={route.path} exact={route.exact} component={route.component} key={route.path} />
-            ))}
-          </Switch>
-        </Router>
-      </SocketProvider>
+      <MainProvider>
+        <UsersProvider>
+          <SocketProvider>
+            <Router>
+              <Header />
+              <Switch>
+                {ROUTES.map((route) => (
+                  <Route path={route.path} exact={route.exact} component={route.component} key={route.path} />
+                ))}
+              </Switch>
+            </Router>
+          </SocketProvider>
+        </UsersProvider>
+      </MainProvider>
     </StylesProvider>
   );
 }
