8f317b0d1f248e7932210c3a410aaae86868a833
JimakPasha <Jimak@tut.by>
2021-09-21 14:20:00 +0300

    fix: corrected app.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,5 @@
 import { StylesProvider } from '@material-ui/core/styles';
 import { Header } from './components/Header/Header';
-import { GamePage } from './pages/GamePage/GamePage';
-import { LobbyPage } from './pages/LobbyPage/LobbyPage';
 import { SocketProvider } from './socketContext';
 
 export function App(): JSX.Element {
@@ -9,8 +7,6 @@ export function App(): JSX.Element {
     <StylesProvider injectFirst>
       <SocketProvider>
         <Header />
-        <GamePage />
-        <LobbyPage />
       </SocketProvider>
     </StylesProvider>
   );
