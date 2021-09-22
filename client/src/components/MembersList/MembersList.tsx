82bd6d6e837cc3f01d70430c14e66403001a8c5f
MaxZay <maximzay2017@gmail.com>
2021-09-22 20:33:20 +0300

    fix: correct all comments

--- client/src/components/MembersList/MembersList.tsx
+++ client/src/components/MembersList/MembersList.tsx
@@ -3,14 +3,14 @@ import { MemberCard } from '../MemberCard/MemberCard';
 import './MembersList.scss';
 
 export const MembersList = (): JSX.Element => {
-  const users = useAppSelector((state) => state.users.users.filter((user) => user.role !== 'scram-master'));
-
+  const users = useAppSelector((state) => state.users.users);
+  const members = users.filter((user) => user.role !== 'scram-master');
   return (
     <div className="member-list">
       <h2 className="member-list__title">Members:</h2>
       <div className="member-list__wrapper">
         {users.length > 0 &&
-          users.map((item) => (
+          members.map((item) => (
             <MemberCard src={item.avatar} name={`${item.firstname} ${item.lastname}`} position={item.position} />
           ))}
       </div>
