ac28398c003010f9d420e541a77f8c9cf7ef1e04
JimakPasha <Jimak@tut.by>
2021-09-24 17:21:00 +0300

    feat: add method deleteUsersInRoom

--- server/users.ts
+++ server/users.ts
@@ -1,6 +1,6 @@
 import { UserModel } from './models/UserModel';
 
-const users: UserModel[] = [];
+let users: UserModel[] = [];
 
 export const addUser = ({ id, firstname, lastname, position, role, avatar, room }: UserModel): UserModel => {
   const user = { id, firstname, lastname, position, role, avatar, room };
@@ -17,6 +17,11 @@ export const deleteUser = (id: string): UserModel | undefined => {
   const index = users.findIndex((user) => user.id === id);
   if (index !== -1) return users.splice(index, 1)[0];
   return undefined;
+};
+
+export const deleteUsersInRoom = (room: string): UserModel[] => {
+  users = users.filter((user) => user.room !== room);
+  return users;
 };
 
 export const getUsers = (room: string): UserModel[] => users.filter((user) => user.room === room);
