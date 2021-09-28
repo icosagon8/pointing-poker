2d8e17bcf0c494b4768c4bf017006012906b50b3
icosagon8 <dmpavlov8@yandex.ru>
2021-09-28 14:03:16 +0300

    fix: hide the chat button on the Home page

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -1,6 +1,8 @@
 import './Header.scss';
+import { useEffect, useState } from 'react';
 import { AppBar, IconButton, Toolbar, Container } from '@material-ui/core';
 import ChatOutlinedIcon from '@material-ui/icons/ChatOutlined';
+import { useLocation } from 'react-router-dom';
 import { useAppSelector, useAppDispatch } from '../../store/hooks/hooks';
 import { Logo } from '../Logo/Logo';
 import { on, off } from '../../store/slices/chatSlice';
@@ -8,17 +10,24 @@ import { on, off } from '../../store/slices/chatSlice';
 export function Header(): JSX.Element {
   const isOpen = useAppSelector((state) => state.chat.isOpen);
   const dispatch = useAppDispatch();
-
   const clickHandler = () => dispatch(isOpen ? off() : on());
+  const location = useLocation();
+  const [isChatButtonDisplay, setChatIsButtonDisplay] = useState<boolean>(false);
+
+  useEffect(() => {
+    setChatIsButtonDisplay(location.pathname !== '/');
+  }, [location]);
 
   return (
     <AppBar className="header" position="static">
       <Container>
         <Toolbar className="header__wrapper" disableGutters>
           <Logo className="header__logo" />
-          <IconButton onClick={clickHandler} className="header__chat-btn" color="inherit">
-            <ChatOutlinedIcon />
-          </IconButton>
+          {isChatButtonDisplay && (
+            <IconButton onClick={clickHandler} className="header__chat-btn" color="inherit">
+              <ChatOutlinedIcon />
+            </IconButton>
+          )}
         </Toolbar>
       </Container>
     </AppBar>
