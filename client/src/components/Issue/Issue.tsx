c43f3ddc17b71cbc8463590ae7d81a2ecfa7182a
JimakPasha <Jimak@tut.by>
2021-10-05 23:25:33 +0300

    fix: stopPropagation for input in issue

--- client/src/components/Issue/Issue.tsx
+++ client/src/components/Issue/Issue.tsx
@@ -73,7 +73,11 @@ export const Issue = (props: IssueModel): JSX.Element => {
 
   const handleChangeScore = (e: React.ChangeEvent<HTMLInputElement>) => {
     setValueScore(e.target.value);
-     socket?.emit('setScoreIssue', id, e.target.value, room);
+    socket?.emit('setScoreIssue', id, e.target.value, room);
+  };
+
+  const handleClickScore = (e: React.MouseEvent<HTMLInputElement>) => {
+    e.stopPropagation();
   };
 
   return (
@@ -84,70 +88,71 @@ export const Issue = (props: IssueModel): JSX.Element => {
       aria-haspopup="true"
       onMouseEnter={handlePopoverOpen}
       onMouseLeave={handlePopoverClose}
-      >
-        <div className="issue__text">
-          {current && location.pathname === '/game' && <span className="issue__text-current">current</span>}
-          <h3 className="issue__text-title">{title}</h3>
-          <p className="issue__text-priority">{priority}</p>
-        </div>
-        <div className="issue__box-score">
-          {user?.role === 'scram-master' && (
-            <>
-              <IconButton className="issue__edit-btn" onClick={handleClickOpen}>
-                <EditIcon />
-              </IconButton>
-              <IconButton onClick={handleClickDelete}>
-                <DeleteOutlineIcon className="issue__delete-btn" />
-              </IconButton>
-              <IssueDialog edit id={id} open={open} onClose={handleClose} />
-            </>
+    >
+      <div className="issue__text">
+        {current && location.pathname === '/game' && <span className="issue__text-current">current</span>}
+        <h3 className="issue__text-title">{title}</h3>
+        <p className="issue__text-priority">{priority}</p>
+      </div>
+      <div className="issue__box-score">
+        {user?.role === 'scram-master' && (
+          <>
+            <IconButton className="issue__edit-btn" onClick={handleClickOpen}>
+              <EditIcon />
+            </IconButton>
+            <IconButton onClick={handleClickDelete}>
+              <DeleteOutlineIcon className="issue__delete-btn" />
+            </IconButton>
+            <IssueDialog edit id={id} open={open} onClose={handleClose} />
+          </>
+        )}
+        <div className="issue__score">
+          {user?.role === 'scram-master' ? (
+            <label className="issue__score-label" htmlFor="score">
+              Score:
+              <input
+                className="issue__score-input"
+                name="score"
+                type="text"
+                value={valueScore}
+                onChange={handleChangeScore}
+                onClick={handleClickScore}
+              />
+            </label>
+          ) : (
+            <p className="issue__score-text">
+              <span className="issue__score-title">Score:</span>
+              {score ? `${score} ${scoreTypeShort}` : '-'}
+            </p>
           )}
-          <div className="issue__score">
-            {user?.role === 'scram-master' ? (
-              <label className="issue__score-label" htmlFor="score">
-                Score:
-                <input
-                  className="issue__score-input"
-                  name="score"
-                  type="text"
-                  value={valueScore}
-                  onChange={handleChangeScore}
-                />
-              </label>
-            ) : (
-              <p className="issue__score-text">
-                <span className="issue__score-title">Score:</span>
-                {score ? `${score} ${scoreTypeShort}` : '-'}
-              </p>
-            )}
-          </div>
         </div>
-        <a className="issue__link" href={link} target="_blank" rel="noreferrer" onClick={handleClickLink}>
-          link on issue {`>`}
-        </a>
-        <Popover
-          id="mouse-over-popover"
-          className={classes.popover}
-          classes={{
-            paper: classes.paper,
-          }}
-          open={openPopover}
-          anchorEl={anchorEl}
-          anchorOrigin={{
-            vertical: 'bottom',
-            horizontal: 'left',
-          }}
-          transformOrigin={{
-            vertical: 'top',
-            horizontal: 'left',
-          }}
-          onClose={handlePopoverClose}
-          disableRestoreFocus
-          disableAutoFocus
-          disableEnforceFocus   
-        >
-          <Typography>{description}</Typography>
-        </Popover>
+      </div>
+      <a className="issue__link" href={link} target="_blank" rel="noreferrer" onClick={handleClickLink}>
+        link on issue {`>`}
+      </a>
+      <Popover
+        id="mouse-over-popover"
+        className={classes.popover}
+        classes={{
+          paper: classes.paper,
+        }}
+        open={openPopover}
+        anchorEl={anchorEl}
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'left',
+        }}
+        transformOrigin={{
+          vertical: 'top',
+          horizontal: 'left',
+        }}
+        onClose={handlePopoverClose}
+        disableRestoreFocus
+        disableAutoFocus
+        disableEnforceFocus
+      >
+        <Typography>{description}</Typography>
+      </Popover>
     </Card>
   );
 };
